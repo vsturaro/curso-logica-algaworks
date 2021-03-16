0c8fdd01ceadccd897f528dd318b695b031505be
Vandercleison Sturaro <vstecnologia@gmail.com>
2021-03-16 09:07:10 -0400

    Exercicio iteração while

---
+++ IteracaoWhile.java
@@ -0,0 +1,35 @@
+package cursoLogica;
+
+public class IteracaoWhile {
+    public static void main(String[] args) {
+        /*for (int i = 0; i < 10; i++){
+            System.out.println("um texto qualquer");
+        }*/
+
+/*        int i = 0;
+        while (i < 10){
+            System.out.println("um texto qualquer");
+            i++;
+        }
+        //		int i = 0;
+//		while (i < 10) {
+//			if (i == 5) {
+//				System.out.println("Vai parar!");
+//				break;
+//			}
+//			System.out.println(i + ": Um texto qualquer.");
+//			i++;
+//		}*/
+        int i = 0;
+        while (i < 10) {
+            if (i == 5) {
+                System.out.println("Vai continuar...");
+                i++;
+                continue;
+            }
+
+            System.out.println(i + ": Um texto qualquer.");
+            i++;
+        }
+    }
+}
