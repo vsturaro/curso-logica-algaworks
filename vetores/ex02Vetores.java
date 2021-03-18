589256c92557e12213982fa3fa5e64524457d3e9
Vandercleison Sturaro <vstecnologia@gmail.com>
2021-03-18 15:07:23 -0400

    Exercicio vetores duas dimensoe

--- vetores/ex02Vetores.java
+++ vetores/ex02Vetores.java
@@ -1,2 +1,36 @@
-package cursoLogica.vetores;public class ex02Vetores {
+package cursoLogica.vetores;
+
+public class ex02Vetores {
+    public static void main(String[] args) {
+
+        Double[] semanaUm = new Double[]{ 1000.0, 1500.0, 1250.0, 2100.0, 1350.0, 970.0, 0.0 };
+        Double[] semanaDois = new Double[]{ 1050.0, 2500.0, 3250.0, 1100.0, 1430.0, 900.0, 0.0 };
+        Double[] semanaTres = new Double[]{ 1700.0, 1300.0, 1450.0, 2000.0, 1390.0, 800.0, 0.0 };
+        Double[] semanaQuatro = new Double[]{ 2100.0, 1600.0, 1850.0, 2100.0, 1220.0, 998.0, 0.0 };
+
+        Double[][] mes = new Double[][] { semanaUm, semanaDois, semanaTres, semanaQuatro };
+
+        Double valorMaiorFaturamento = 0.0;
+        Integer semanaMaiorFaturamento = null;
+
+
+        for(int i = 0; i < mes.length; i++) {
+            Double[] semana = mes[i]; //cria array semana, para numerar a array mes
+            Double valorFaturamentoSemana = 0.0;
+
+            for (int y = 0; y < semana.length; y++) {
+                valorFaturamentoSemana += semana[y];
+            }
+            Boolean faturamentoMaior = valorFaturamentoSemana > valorMaiorFaturamento;
+            if (faturamentoMaior) {
+                valorMaiorFaturamento = valorFaturamentoSemana;
+                semanaMaiorFaturamento = i + 1;
+            }
+
+        }
+            System.out.println("A semana " + semanaMaiorFaturamento + " teve o maior faturamento que foi de no valor de R$: " + valorMaiorFaturamento);
+
+    }
 }
+
+
