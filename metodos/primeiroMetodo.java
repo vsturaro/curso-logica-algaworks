cc5ce61052d0e8472e4f915e1500e88f3726e496
Vandercleison Sturaro <vstecnologia@gmail.com>
2021-03-22 08:19:05 -0400

    Estudando metodos

---
+++ metodos/primeiroMetodo.java
@@ -0,0 +1,86 @@
+package cursoLogica.metodos;
+
+import java.util.Scanner;
+
+public class primeiroMetodo {
+
+    static Boolean posicaoValida;
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            imprimirTraco();
+
+            String[] cursos = new String[] {"Java EE", "Spring", "Java OO Avançado"};
+
+            System.out.println("Escolha dentre os cursos abaixo: ");
+
+/*            for(int i = 0; i < cursos.length; i++) {
+                System.out.println("[" + i + "] " + cursos[i]);
+            }*/
+            iterarExibirPosicoes(cursos);
+
+            System.out.print("O curso que você deseja é o: ");
+            Integer posicaoCursoEscolhido = scanner.nextInt();
+
+            posicaoValida = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
+
+/*            if (!posicaoValida) { //substituido pelo método validarOpcao
+                System.err.println("Posição inválida!");
+                System.exit(1);
+
+            }*/
+            validarOpcao();
+            imprimirTraco();
+
+            String[] formasPagamento = new String[] {"Cartão", "Boleto"};
+
+            System.out.println("Escolha dentre as formas de pagamento abaixo: ");
+
+/*            for(int i = 0; i < formasPagamento.length; i++) {
+                System.out.println("[" + i + "] " + formasPagamento[i]);
+            }*/
+            iterarExibirPosicoes(formasPagamento);
+
+            System.out.print("Sua forma de pagamento escolhida é: ");
+            Integer posicaoFormaPagamentoEscolhida = scanner.nextInt();
+
+            posicaoValida = posicaoFormaPagamentoEscolhida >= 0
+                    && posicaoFormaPagamentoEscolhida < formasPagamento.length;
+
+            /*if (!posicaoValida) {
+                System.err.println("Posição inválida!");
+                System.exit(1);
+                }
+*/
+                validarOpcao();
+
+
+            String cursoEscolhido = cursos[posicaoCursoEscolhido];
+            String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+
+            imprimirTraco();
+
+            System.out.println("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhida);
+
+            scanner.close();
+        }
+        static void iterarExibirPosicoes(String[] array){
+            for(int i = 0; i < array.length; i++) {
+                System.out.println("[" + i + "] " + array[i]);
+            }
+        }
+
+
+        static void imprimirTraco() {
+            System.out.println("----------------------------------------------");
+        }
+
+        static void validarOpcao(){
+            if(!posicaoValida) {
+                System.err.println("Posição inválida!");
+                System.exit(0);
+            }
+        }
+}
