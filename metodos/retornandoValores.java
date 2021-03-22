ffb6c6e9cfacfbbf875a12dab8e154554ba65a04
vsturaro <vstecnologia@gmail.com>
2021-03-22 09:32:52 -0400

    Exemplo simples de retornar valor em metodo

---
+++ metodos/retornandoValores.java
@@ -0,0 +1,12 @@
+package cursoLogica.metodos;
+
+public class retornandoValores {
+    public static void main(String[] args) {
+        Integer numeroQualquer = numeroAleatorio();
+        System.out.println("Numero: "+ numeroQualquer);
+    }
+    static Integer numeroAleatorio(){
+        Integer numero = 1;
+        return numero;
+    }
+}
