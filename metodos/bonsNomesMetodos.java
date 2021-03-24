4e8f632c6635be354542e17ea90204fc9e11c744
vsturaro <vstecnologia@gmail.com>
2021-03-24 07:40:29 -0400

    Refatorando para torar ainda mais recursivo

--- metodos/bonsNomesMetodos.java
+++ metodos/bonsNomesMetodos.java
@@ -1,51 +1,57 @@
 package cursoLogica.metodos;
 import java.util.Scanner;
 
-public class recursividade {
+public class bonsNomesMetodos {
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
             imprimirTraco();
 
-            String[] cursos = new String[] {"Java EE", "Spring", "Java OO Avançado"};
-
-            imprimir("Escolha dentre os cursos abaixo: ");
-
-            iterarEExibirPosicoesDoVetorString(cursos);
-
-            Integer posicaoCursoEscolhido = receberNumeroInteiroDoUsuario("O curso que você deseja é o: ", scanner);
-
-            Boolean posicaoValida = verificarPosicaoEscolhidaPeloUsuario(posicaoCursoEscolhido, cursos);
-
-            if (!posicaoValida) {
-                encerrarProgramaPorCausaDaPosicaoInvalida();
-            }
+            String cursoEscolhido = receberCursoDoUsuario(scanner);
 
             imprimirTraco();
 
-            String[] formasPagamento = new String[] {"Cartão", "Boleto"};
-
-            imprimir("Escolha dentre as formas de pagamento abaixo: ");
-
-            iterarEExibirPosicoesDoVetorString(formasPagamento);
-
-            Integer posicaoFormaPagamentoEscolhida = receberNumeroInteiroDoUsuario("Sua forma de pagamento escolhida é: ", scanner);
-
-            posicaoValida = verificarPosicaoEscolhidaPeloUsuario(posicaoFormaPagamentoEscolhida, formasPagamento);
-
-            if (!posicaoValida) {
-                encerrarProgramaPorCausaDaPosicaoInvalida();
-            }
-
-            String cursoEscolhido = cursos[posicaoCursoEscolhido];
-            String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+            String formaPagamentoEscolhida = receberFormaPagamentoUsuario(scanner);
 
             imprimirTraco();
 
             imprimir("O curso escolhido foi " + cursoEscolhido + " e a forma de pagamento é " + formaPagamentoEscolhida);
 
             scanner.close();
+        }
+        static String receberFormaPagamentoUsuario(Scanner scanner){
+            String[] formasPagamento = new String[] {"Cartão", "Boleto"};
+
+            imprimir("Escolha dentre as formas de pagamento abaixo: ");
+
+            iterarEExibirPosicoesDoVetorString(formasPagamento);
+
+            Integer posicaoFormaPagamentoEscolhida = receberIndeceValidoDoUsuario(formasPagamento, "Sua forma de pagamento escolhida é: ", scanner);
+
+            return formasPagamento[posicaoFormaPagamentoEscolhida];
+        }
+
+        static String receberCursoDoUsuario(Scanner scanner){
+            String[] cursos = new String[] {"Java EE", "Spring", "Java OO Avançado"};
+
+            imprimir("Escolha dentre os cursos abaixo: ");
+
+            iterarEExibirPosicoesDoVetorString(cursos);
+
+            Integer posicaoCursoEscolhido = receberIndeceValidoDoUsuario(cursos, "O curso que você deseja é o: ", scanner);
+            return cursos[posicaoCursoEscolhido];
+        }
+
+        static Integer receberIndeceValidoDoUsuario(String[] vetor, String  texto, Scanner scanner){
+            Integer posicaoEscolhida = receberNumeroInteiroDoUsuario(texto, scanner);
+
+            Boolean posicaoValida = verificarPosicaoEscolhidaPeloUsuario(posicaoEscolhida, vetor);
+
+            if (!posicaoValida) {
+                encerrarProgramaPorCausaDaPosicaoInvalida();
+            }
+            return posicaoEscolhida;
         }
 
         static Integer receberNumeroInteiroDoUsuario(String texto, Scanner scanner) {
