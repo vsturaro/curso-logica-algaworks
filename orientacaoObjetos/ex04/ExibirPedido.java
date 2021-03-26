c79463d0aa6bafd0c9131d7bf70339718ff3d55f
vsturaro <vstecnologia@gmail.com>
2021-03-25 22:06:00 -0400

    Exercício prático usando encapsulamento

--- orientacaoObjetos/ex04/ExibirPedido.java
+++ orientacaoObjetos/ex04/ExibirPedido.java
@@ -1,2 +1,17 @@
-package cursoLogica.orientacaoObjetos.ex04;public class ExibirPedido {
+package cursoLogica.orientacaoObjetos.ex04;
+
+public class ExibirPedido {
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido();
+
+        pedido.setCodigo(10);
+        pedido.setDescontoPercentual(5.0);
+        pedido.setSubtotal(300.0);
+        pedido.setTotal();
+
+        imprimir("Pedido nº: "+pedido.getCodigo()+" Subtotal R$ "+pedido.getSubtotal()+" Desconto à vista "+pedido.getDescontoPercentual()+"% (R$ "+(pedido.getSubtotal() * pedido.getDescontoPercentual())/100+"), Total R$ "+pedido.getTotal());
+    }
+    static void imprimir(String texto){
+        System.out.println(texto);
+    }
 }
--- orientacaoObjetos/ex04/Pedido.java
+++ orientacaoObjetos/ex04/Pedido.java
@@ -1,2 +1,46 @@
-package cursoLogica.orientacaoObjetos.ex04;public class Pedido {
+package cursoLogica.orientacaoObjetos.ex04;
+
+public class Pedido {
+
+    Integer codigo;
+    Double subtotal;
+    Double descontoPercentual;
+    //Double total;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Double getDescontoPercentual() {
+        return descontoPercentual;
+    }
+
+    public void setDescontoPercentual(Double descontoPercentual) {
+        this.descontoPercentual = descontoPercentual;
+    }
+
+    public Double getTotal() {
+        return subtotal - (subtotal * (descontoPercentual / 100));
+    }
+
+    public void setTotal() {
+
+    }
+
+/*    public void setTotal(Double total) {
+        total ;
+    }*/
+
 }
